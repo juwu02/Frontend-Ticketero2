f54bd056889d4048ea4f497baef361f109d10a41
jule <a@test.com>
2023-01-09 15:44:41 +0100

    user service hinzugefügt

--- src/app/skipass.ts
+++ src/app/skipass.ts
@@ -3,3 +3,23 @@ export interface Skipass {
   vorname: String,
   nachname: String
 }
+
+/*
+ticket_id:
+name:
+valid_from
+valid_to:
+price:
+amount:
+location:
+description:
+ */
+
+/*
+serial_no:
+firstname:
+lastname:
+birthdate:
+user_id:
+ticket_id:
+ */
